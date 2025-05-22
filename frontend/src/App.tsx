63ea10c74d0363fadd72d0e74721f6a32110fb71
Mayank Rai <Mayank.dks@gmail.com>
2025-05-22 13:32:41 +0530

    settedup initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to React + TypeScript + Webpack</h1>
+      <p>Your application is now set up!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+};
